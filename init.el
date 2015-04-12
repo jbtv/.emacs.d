6ef00c6d74e2adbcacdb983bdbfa48c70086c45b
Blake Miller <blake@tengrade.com>
2015-04-12 00:19:33 -0700

    add binding to jump to clojure source
    even works jumping to source in a maven jar! holy fuck

--- init.el
+++ init.el
@@ -382,6 +382,7 @@ _b_: prev
       (cider-interactive-eval "(reloaded.repl/reset)"))
 
     (evil-leader/set-key "cd" 'cider-doc)
+    (evil-leader/set-key "cj" 'cider-jump-to-var)
     (evil-leader/set-key "cc" 'cider-connect)
     (evil-leader/set-key "ct" 'cider-test-run-tests)
     (evil-leader/set-key "cr" 'toggle-nrepl-buffer)
