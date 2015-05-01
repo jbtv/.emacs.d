317fde0a57707b727c62d74129102963503547c4
blak3mill3r <blak3mill3r@gmail.com>
2015-05-01 15:28:43 -0700

    add binding for mongo-local-staging

--- init.el
+++ init.el
@@ -87,6 +87,7 @@
   (progn
     (evil-leader/set-key ",mipro" (lambda () (interactive) (inf-mongo mongo-iris-prod-read-only)))
     (evil-leader/set-key ",mid" (lambda () (interactive) (inf-mongo mongo-iris-dev)))
+    (evil-leader/set-key ",mils" (lambda () (interactive) (inf-mongo mongo-iris-local-staging)))
     (evil-leader/set-key ",mis" (lambda () (interactive) (inf-mongo mongo-iris-staging))) 
     
     ))
